dda16c6f8eb59ac501bd73d1d62729aca9b34a51
GLR-PC-0 <info@glracing.de>
2024-06-08 14:14:14 +0200

    wip nice way to log changes in sponsors/members
    database migration done
    
    Co-Authored-By: wallalex0 <97884125+wallalex0@users.noreply.github.com>

--- databaseporting.cpp
+++ databaseporting.cpp
@@ -66,32 +66,32 @@ DatabasePorting::DatabasePorting() {
                          "FOREIGN KEY('personen_id') REFERENCES 'personen'('id'))");
         QSqlQuery Query5("INSERT INTO kommunikationen_temp SELECT id, FirmenID, PersonenID, wann, was FROM kommunikationen");
 
-        QSqlQuery Query26("SELECT id, firma, ansprechpartner FROM kommunikationen");
-        QSqlQuery Query27("SELECT id, vorname, nachname FROM personen");
-        QSqlQuery Query28;
+        QSqlQuery Query6("SELECT id, firma, ansprechpartner FROM kommunikationen");
+        QSqlQuery Query7("SELECT id, vorname, nachname FROM personen");
+        QSqlQuery Query8;
 
-        while (Query26.next()) {
-            Query27.exec();
-            while (Query27.next()) {
-                if (Query26.value(2) == Query27.value(1).toString() + " " + Query27.value(2).toString()) {
-                    Query28.prepare("UPDATE kommunikationen_temp SET personen_id = :personen_id WHERE id = :id");
-                    Query28.bindValue(":id", Query26.value(0));
-                    Query28.bindValue(":personen_id", Query27.value(0));
-                    Query28.exec();
+        while (Query6.next()) {
+            Query7.exec();
+            while (Query7.next()) {
+                if (Query6.value(2) == Query7.value(1).toString() + " " + Query7.value(2).toString()) {
+                    Query8.prepare("UPDATE kommunikationen_temp SET personen_id = :personen_id WHERE id = :id");
+                    Query8.bindValue(":id", Query6.value(0));
+                    Query8.bindValue(":personen_id", Query7.value(0));
+                    Query8.exec();
                 }
             }
         }
 
-        QSqlQuery Query6("DROP TABLE kommunikationen");
-        QSqlQuery Query7("ALTER TABLE kommunikationen_temp RENAME TO kommunikationen");
+        QSqlQuery Query9("DROP TABLE kommunikationen");
+        QSqlQuery Query10("ALTER TABLE kommunikationen_temp RENAME TO kommunikationen");
 
         qDebug() << "4  " + Query4.lastError().text();
         qDebug() << "5  " + Query5.lastError().text();
         qDebug() << "6  " + Query6.lastError().text();
         qDebug() << "7  " + Query7.lastError().text();
-        qDebug() << "26  " + Query26.lastError().text();
-        qDebug() << "27  " + Query27.lastError().text();
-        qDebug() << "28  " + Query28.lastError().text();
+        qDebug() << "8  " + Query8.lastError().text();
+        qDebug() << "9  " + Query9.lastError().text();
+        qDebug() << "10  " + Query10.lastError().text();
     }
 
 
@@ -103,7 +103,7 @@ DatabasePorting::DatabasePorting() {
     //                   to id, firmen_id, titel, vorname, nachname, position, telefon, email, du_sie, sprache, aktiv in personen
     current = checkTable("personen", "id INTEGER, firmen_id INTEGER, titel TEXT, vorname TEXT, nachname TEXT, position TEXT, telefon TEXT, email TEXT, du_sie TEXT, sprache TEXT, aktiv BOOLEAN");
     if (!current) {
-        QSqlQuery Query8("CREATE TABLE personen_temp ("
+        QSqlQuery Query11("CREATE TABLE personen_temp ("
                          "id INTEGER,"
                          "firmen_id INTEGER,"
                          "titel TEXT,"
@@ -117,14 +117,14 @@ DatabasePorting::DatabasePorting() {
                          "aktiv BOOLEAN,"
                          "PRIMARY KEY('id'),"
                          "FOREIGN KEY('firmen_id') REFERENCES 'firmen'('id'))");
-        QSqlQuery Query9("INSERT INTO personen_temp SELECT id, FirmenID, titel, vorname, nachname, Position, telefon, email, du_sie, sprache, aktiv FROM personen");
-        QSqlQuery Query10("DROP TABLE personen");
-        QSqlQuery Query11("ALTER TABLE personen_temp RENAME TO personen");
+        QSqlQuery Query12("INSERT INTO personen_temp SELECT id, FirmenID, titel, vorname, nachname, Position, telefon, email, du_sie, sprache, aktiv FROM personen");
+        QSqlQuery Query13("DROP TABLE personen");
+        QSqlQuery Query14("ALTER TABLE personen_temp RENAME TO personen");
 
-        qDebug() << "8  " + Query8.lastError().text();
-        qDebug() << "9  " + Query9.lastError().text();
-        qDebug() << "10 " + Query10.lastError().text();
-        qDebug() << "11 " + Query11.lastError().text();
+        qDebug() << "11  " + Query11.lastError().text();
+        qDebug() << "12  " + Query12.lastError().text();
+        qDebug() << "13 " + Query13.lastError().text();
+        qDebug() << "14 " + Query14.lastError().text();
     }
 
 
@@ -133,7 +133,7 @@ DatabasePorting::DatabasePorting() {
     // reorder columns  from id, firmen_id, personen_id, wann, wert, was, infos to id, firmen_id, personen_id, wann, was, wert, infos in leistungen
     current = checkTable("leistungen", "id INTEGER, firmen_id INTEGER, personen_id INTEGER, wann TEXT, was TEXT, wert TEXT, infos TEXT");
     if (!current) {
-        QSqlQuery Query12("CREATE TABLE leistungen_temp ("
+        QSqlQuery Query15("CREATE TABLE leistungen_temp ("
                           "id INTEGER,"
                           "firmen_id INTEGER,"
                           "personen_id INTEGER,"
@@ -144,45 +144,45 @@ DatabasePorting::DatabasePorting() {
                           "PRIMARY KEY('id'),"
                           "FOREIGN KEY('firmen_id') REFERENCES 'firmen'('id'),"
                           "FOREIGN KEY('personen_id') REFERENCES 'personen'('id'))");
-        QSqlQuery Query13("INSERT INTO leistungen_temp SELECT id, firma, ansprechpartner, wann, was, wert, infos FROM leistungen");
+        QSqlQuery Query16("INSERT INTO leistungen_temp SELECT id, firma, ansprechpartner, wann, was, wert, infos FROM leistungen");
 
-        QSqlQuery Query14("SELECT id, firma, ansprechpartner FROM leistungen");
-        QSqlQuery Query15("SELECT id, name FROM firmen");
-        QSqlQuery Query16("SELECT id, vorname, nachname FROM personen");
-        QSqlQuery Query17;
+        QSqlQuery Query17("SELECT id, firma, ansprechpartner FROM leistungen");
+        QSqlQuery Query18("SELECT id, name FROM firmen");
+        QSqlQuery Query19("SELECT id, vorname, nachname FROM personen");
+        QSqlQuery Query20;
 
-        while (Query14.next()) {
-            Query15.exec();
-            while (Query15.next()) {
-                if (Query14.value(1) == Query15.value(1)) {
-                    Query17.prepare("UPDATE leistungen_temp SET firmen_id = :firmen_id WHERE id = :id");
-                    Query17.bindValue(":id", Query14.value(0));
-                    Query17.bindValue(":firmen_id", Query15.value(0));
-                    Query17.exec();
+        while (Query17.next()) {
+            Query18.exec();
+            while (Query18.next()) {
+                if (Query17.value(1) == Query18.value(1)) {
+                    Query20.prepare("UPDATE leistungen_temp SET firmen_id = :firmen_id WHERE id = :id");
+                    Query20.bindValue(":id", Query17.value(0));
+                    Query20.bindValue(":firmen_id", Query18.value(0));
+                    Query20.exec();
                 }
             }
-            Query16.exec();
-            while (Query16.next()) {
-                if (Query14.value(2) == Query16.value(1).toString() + " " + Query16.value(2).toString()) {
-                    Query17.prepare("UPDATE leistungen_temp SET personen_id = :personen_id WHERE id = :id");
-                    Query17.bindValue(":id", Query14.value(0));
-                    Query17.bindValue(":personen_id", Query16.value(0));
-                    Query17.exec();
+            Query19.exec();
+            while (Query19.next()) {
+                if (Query17.value(2) == Query19.value(1).toString() + " " + Query19.value(2).toString()) {
+                    Query20.prepare("UPDATE leistungen_temp SET personen_id = :personen_id WHERE id = :id");
+                    Query20.bindValue(":id", Query17.value(0));
+                    Query20.bindValue(":personen_id", Query19.value(0));
+                    Query20.exec();
                 }
             }
         }
 
-        QSqlQuery Query18("DROP TABLE leistungen");
-        QSqlQuery Query19("ALTER TABLE leistungen_temp RENAME TO leistungen");
+        QSqlQuery Query21("DROP TABLE leistungen");
+        QSqlQuery Query22("ALTER TABLE leistungen_temp RENAME TO leistungen");
 
-        qDebug() << "12 " + Query12.lastError().text();
-        qDebug() << "13 " + Query13.lastError().text();
-        qDebug() << "14 " + Query14.lastError().text();
         qDebug() << "15 " + Query15.lastError().text();
         qDebug() << "16 " + Query16.lastError().text();
         qDebug() << "17 " + Query17.lastError().text();
         qDebug() << "18 " + Query18.lastError().text();
         qDebug() << "19 " + Query19.lastError().text();
+        qDebug() << "20 " + Query20.lastError().text();
+        qDebug() << "21 " + Query21.lastError().text();
+        qDebug() << "22 " + Query22.lastError().text();
     }
 
 
@@ -191,9 +191,10 @@ DatabasePorting::DatabasePorting() {
     // rename column Leistungstyp to leistungstyp in firmen
     // rename column Infos to infos in firmen
     // reorder columns from id, name, aktiv, seit, bis, rang, leistungstyp, anschrift, infos to id, name, seit, bis, rang, leistungstyp, infos, anschrift, aktiv in firmen
-    current = checkTable("firmen", "id INTEGER, name TEXT, seit TEXT, bis TEXT, rang TEXT, leistungstyp TEXT, infos TEXT, anschrift TEXT, aktiv BOOLEAN");
+    current = checkTable("firmen", "id INTEGER, name TEXT, seit TEXT, bis TEXT, rang TEXT, leistungstyp TEXT, infos TEXT, anschrift TEXT, aktiv BOOLEAN") ||
+        checkTable("firmen", "id INTEGER, name TEXT, infos TEXT, anschrift TEXT, aktiv BOOLEAN");
     if (!current) {
-        QSqlQuery Query20("CREATE TABLE firmen_temp ("
+        QSqlQuery Query23("CREATE TABLE firmen_temp ("
                           "id INTEGER,"
                           "name TEXT,"
                           "seit TEXT,"
@@ -204,30 +205,128 @@ DatabasePorting::DatabasePorting() {
                           "anschrift TEXT,"
                           "aktiv BOOLEAN,"
                           "PRIMARY KEY('id'))");
-        QSqlQuery Query21("INSERT INTO firmen_temp SELECT id, name, seit, bis, Rang, Leistungstyp, Infos, Land, aktiv FROM firmen");
+        QSqlQuery Query24("INSERT INTO firmen_temp SELECT id, name, seit, bis, Rang, Leistungstyp, Infos, Land, aktiv FROM firmen");
 
-        QSqlQuery Query22("SELECT id, Str, Hausnummer, Ort, PLZ, Land FROM firmen");
-        QSqlQuery Query23;
+        QSqlQuery Query25("SELECT id, Str, Hausnummer, Ort, PLZ, Land FROM firmen");
+        QSqlQuery Query26;
 
-        while (Query22.next()) {
-            Query23.prepare("UPDATE firmen_temp SET anschrift = :anschrift WHERE id = :id");
-            Query23.bindValue(":id", Query22.value(0));
-            Query23.bindValue(":anschrift", Query22.value(5).toString()+ ", " + Query22.value(3).toString() + ", " + Query22.value(4).toString()+ ", " + Query22.value(1).toString() + " " + Query22.value(2).toString());
-            Query23.exec();
+        while (Query25.next()) {
+            Query26.prepare("UPDATE firmen_temp SET anschrift = :anschrift WHERE id = :id");
+            Query26.bindValue(":id", Query25.value(0));
+            Query26.bindValue(":anschrift", Query25.value(5).toString()+ ", " + Query25.value(3).toString() + ", " + Query25.value(4).toString()+ ", " + Query25.value(1).toString() + " " + Query25.value(2).toString());
+            Query26.exec();
         }
 
-        QSqlQuery Query24("DROP TABLE firmen");
-        QSqlQuery Query25("ALTER TABLE firmen_temp RENAME TO firmen");
+        QSqlQuery Query27("DROP TABLE firmen");
+        QSqlQuery Query28("ALTER TABLE firmen_temp RENAME TO firmen");
 
-        qDebug() << "20 " + Query20.lastError().text();
-        qDebug() << "21 " + Query21.lastError().text();
-        qDebug() << "22 " + Query22.lastError().text();
         qDebug() << "23 " + Query23.lastError().text();
         qDebug() << "24 " + Query24.lastError().text();
         qDebug() << "25 " + Query25.lastError().text();
+        qDebug() << "26 " + Query26.lastError().text();
+        qDebug() << "27 " + Query27.lastError().text();
+        qDebug() << "28 " + Query28.lastError().text();
     }
 
-    // ******* changes to database format after , commit  *******
+    // ******* changes to database format after 2024-06-08, commit 0739f09 *******
+
+    // add tables to log changes in members and companys
+    QSqlQuery CompanyChangesQquery("CREATE TABLE IF NOT EXISTS firmen_aenderungen (id INTEGER,"
+                                   "firmen_id INTEGER,"
+                                   "von TEXT,"
+                                   "bis TEXT,"
+                                   "rang TEXT,"
+                                   "leistungstyp TEXT,"
+                                   "PRIMARY KEY('id'),"
+                                   "FOREIGN KEY('firmen_id') REFERENCES 'firmen'('id'))");
+    QSqlQuery MemberChangesQuery("CREATE TABLE IF NOT EXISTS mitglieder_aenderungen (id INTEGER,"
+                                 "mitglieder_id INTEGER,"
+                                 "von TEXT,"
+                                 "bis TEXT,"
+                                 "position TEXT,"
+                                 "PRIMARY KEY('id'),"
+                                 "FOREIGN KEY('mitglieder_id') REFERENCES 'mitglieder'('id'))");
+
+    // remove columns von, bis, rang, leistungstyp from firmen
+    // integrate into firmen_aenderungen
+    current = checkTable("firmen", "id INTEGER, name TEXT, infos TEXT, anschrift TEXT, aktiv BOOLEAN");
+    if (!current) {
+        QSqlQuery Query29("CREATE TABLE firmen_temp ("
+                          "id INTEGER,"
+                          "name TEXT,"
+                          "infos TEXT,"
+                          "anschrift TEXT,"
+                          "aktiv BOOLEAN,"
+                          "PRIMARY KEY('id'))");
+        QSqlQuery Query30("INSERT INTO firmen_temp SELECT id, name, infos, anschrift, aktiv FROM firmen");
+
+        QSqlQuery Query31("SELECT id, seit, bis, rang, leistungstyp FROM firmen");
+        QSqlQuery Query32;
+
+        while (Query31.next()) {
+            Query32.prepare("INSERT INTO firmen_aenderungen (firmen_id, von, bis, rang, leistungstyp) VALUES (:company_id, :from, :to, :rank, :type);");
+            Query32.bindValue(":company_id", Query31.value(0).toInt());
+            Query32.bindValue(":from", Query31.value(1).toString());
+            Query32.bindValue(":to", Query31.value(2).toString());
+            Query32.bindValue(":rank", Query31.value(3).toString());
+            Query32.bindValue(":type", Query31.value(4).toString());
+            Query32.exec();
+        }
+
+        QSqlQuery Query33("DROP TABLE firmen");
+        QSqlQuery Query34("ALTER TABLE firmen_temp RENAME TO firmen");
+
+        qDebug() << "29 " + Query29.lastError().text();
+        qDebug() << "30 " + Query30.lastError().text();
+        qDebug() << "31 " + Query31.lastError().text();
+        qDebug() << "32 " + Query32.lastError().text();
+        qDebug() << "33 " + Query33.lastError().text();
+        qDebug() << "34 " + Query34.lastError().text();
+    }
+
+    // remove columns position from mitglieder
+    // integrate into mitglieder_aenderungen
+    current = checkTable("mitglieder", "id INTEGER, vorname TEXT, nachname TEXT, matrikelnummer TEXT, email_glr TEXT, email_privat TEXT, telefon TEXT, anschrift TEXT, hemdgroesse TEXT, vdi_nummer TEXT, sprache TEXT, aktiv BOOL");
+    if (!current) {
+        QSqlQuery Query35("CREATE TABLE IF NOT EXISTS mitglieder_temp ("
+                          "id INTEGER PRIMARY KEY, "
+                          "vorname TEXT, "
+                          "nachname TEXT, "
+                          "matrikelnummer TEXT, "
+                          "email_glr TEXT, "
+                          "email_privat TEXT, "
+                          "telefon TEXT, "
+                          "anschrift TEXT, "
+                          "hemdgroesse TEXT, "
+                          "vdi_nummer TEXT, "
+                          "sprache TEXT, "
+                          "aktiv BOOL)");
+        QSqlQuery Query36("INSERT INTO mitglieder_temp SELECT id, vorname, nachname, matrikelnummer, email_glr, email_privat, telefon, anschrift, hemdgroesse, vdi_nummer, sprache, aktiv FROM mitglieder");
+
+        QSqlQuery Query37("SELECT id, position FROM mitglieder");
+        QSqlQuery Query38;
+
+        while (Query37.next()) {
+            Query38.prepare("INSERT INTO mitglieder_aenderungen (mitglieder_id, von, bis, position) VALUES (:member_id, :from, :to, :position)");
+            Query38.bindValue(":member_id", Query37.value(0).toString());
+            Query38.bindValue(":from", "");
+            Query38.bindValue(":to", "");
+            Query38.bindValue(":position", Query37.value(1).toString());
+            Query38.exec();
+        }
+
+        QSqlQuery Query39("DROP TABLE mitglieder");
+        QSqlQuery Query40("ALTER TABLE mitglieder_temp RENAME TO mitglieder");
+
+        qDebug() << "35 " + Query35.lastError().text();
+        qDebug() << "36 " + Query36.lastError().text();
+        qDebug() << "37 " + Query37.lastError().text();
+        qDebug() << "38 " + Query38.lastError().text();
+        qDebug() << "39 " + Query39.lastError().text();
+        qDebug() << "40 " + Query40.lastError().text();
+    }
+
+    // ******* changes to database format after 0000-00-00, commit 000000 *******
 }
 
 bool DatabasePorting::checkTable(QString tableName, QString tableStructure) {
